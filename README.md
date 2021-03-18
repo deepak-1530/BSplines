7bda2cfb439c076e1a528272ad197b9ea32dbff6
Deepak singh <deepaks.ec.16@nsit.net.in>
2021-03-19 01:54:29 +0530

    Basic B-spline code added.

---
+++ README.md
@@ -0,0 +1,28 @@
+## B-Spline Trajectory Generation
+
+
+### 1. Introduction
+B-Spline trajectory generation and optimization for autonomous navigation for drones. 
+
+### 2. Pre-requisites
+Eigen, C++11
+
+### 3. Running the code
+bspline.h contains the implementation of cox-deBoor recursive formula for calculation of splines.
+
+git clone https://github.com/deepak-1530/BSplines.git
+
+mkdir build
+
+cd build
+
+cmake .. && make
+
+./main 
+
+#### Plotting the results
+Running the executable creates two files -> data.txt and dataControlPoints.txt
+
+These are used in the python script (plotSpline.py)
+
+python3 plotSpline.py
---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include"bspline.h"
+
+using namespace std;
+
+BSpline::BSpline bspline;  // bspline object
+
+std::vector<Eigen::Vector3d> controlPoints; 
+
+int splineOrder = 5;
+
+
+int main()
+{
+    std::ofstream splineData;
+
+    std::cout<<"BSpline fitting on points in space ..."<<std::endl;
+
+    std::cout<<"Enter the num of control points ";
+    std::cin>>bspline.numCtrlPoints;
+
+    splineData.open("dataControlPoints.txt");
+
+    // take the points as input
+    for(int i = 0; i<=bspline.numCtrlPoints; i++)
+    {
+        std::cout<<"Enter the coordinate "<<i<<" ";
+        Eigen::Vector3d pt;
+        std::cin>>pt(0)>>pt(1)>>pt(2);
+        splineData<<pt(0)<<","<<pt(1)<<","<<pt(2)<<std::endl;
+        controlPoints.push_back(pt);
+    }
+    splineData.close();
+    // set this as the control points
+    bspline.setControlPoints(controlPoints);
+
+    // set the order
+    bspline.setOrder(splineOrder);
+
+    // now calculate the spline
+    bspline.setKnotVector();    // this sets the knot vector
+
+    // set the no. of segments
+    bspline.setNumSegments();
+
+    // calculate the bspline points
+    bspline.getBSplineTrajectory();
+
+    // print the spline points
+
+    splineData.open("data.txt");
+
+    for(int i = 0; i<bspline.splineSegments.size(); i++)
+    {
+        std::vector<Eigen::Vector3d> spPts = bspline.splineSegments[i];
+
+        for(int i = 0; i<spPts.size(); i++)
+        {
+            Eigen::Vector3d point;
+            std::cout<<spPts[i].transpose()<<std::endl;
+            point = spPts[i];
+            splineData<<point(0)<<","<<point(1)<<","<<point(2)<<std::endl;
+        }
+    }
+
+    splineData.close();
+
+    return 0;
+
+}
